b4a0f890acdb4fd81a722de29a09b2de0249ada1
Peter Epp <peter@teknocat.org>
2013-04-11 20:49:33 -0600

    Updated to Biscuit 2.2 version

--- extension.php
+++ extension.php
@@ -3,10 +3,11 @@
  * Extension that adds the ie6update js script from ie6update.com to the page footer
  *
  * @package Extensions
+ * @subpackage Ie6Update
  * @author Peter Epp
  * @copyright Copyright (c) 2009 Peter Epp (http://teknocat.org)
  * @license GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.html)
- * @version 2.0
+ * @version 2.0 $Id: extension.php 13982 2011-08-08 18:51:55Z teknocat $
  */
 class Ie6Update extends AbstractExtension {
 	public function run() {
